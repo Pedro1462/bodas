d0404194f6005d08dd136135ca6b7f1c89de3899
pedro1462 <pedrojosueriverocanche@gmail.com>
2024-11-24 22:07:08 -0500

    inicio de la principal pruebas

--- bodas/MVC/vista/principal.php
+++ bodas/MVC/vista/principal.php
@@ -6,61 +6,116 @@
     <link rel="stylesheet" href="../../css/bodas.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
 
-    <title>Mixología CDMX</title>
+
+    <title>El gran día</title>
 
 </head>
+
 <body>
-<div class="parent">
-<div class="div1">
-    <h1>Bodas si señor</h1>
-</div>
+    <div class="parent">
 
-
-
-<div class="div2">
- <h2> Invento</h2>
-</div>
-
-
-
-<div class="div3"> 
-    <img src="../../img/img1.jpg" alt="img1">
-</div>
-
-
-
-
-<div class="div5">
-<img src="../../img/img3.jpg" alt="img3">
-</div>
-
-
-<div class="div6"> 
-<h3>Contacto</h3>
-</div>
-
-<div class="div7"> 
-    <img src="../../../img/img2.jpg" alt="img2">
-</div>
-
-<div class="div4"> 
-
-
-
-<h2>Referencias</h2>
-
-
-
-
-
-
-
-
-</div>
-
-</div>
     
+        <div class="div1">
+            <p class="presentacion">Encuentra todo para tus eventos en un solo lugar</p>
+        </div>
+
+
+
+        <div class="div2">
+
+            <div class="row">
+                <h1 class="centrar">
+                    <img src="../../img/logo1.png" class="logo" alt="logo1">
+                    <a href="index.php"><strong>El gran dia</strong></a>
+
+                </h1>
+            </div>
+
+            <div class="col">
+                <div class="btn-group">
+                    <button class="btn btn-personalizado" onclick="location.href='?c=nuevoMenu';">Menú
+
+                    </button>
+
+                    <button class="btn btn-personalizado" onclick="location.href='?c=promos';">Cotizacion
+
+                    </button>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="btn-group">
+
+                    <button class="btn btn-personalizado" onclick="location.href='?c=login';">
+                        <img src="../../img/img4.png" class="logo" alt="img4"> Iniciar Sesión
+                    </button>
+
+                </div>
+            </div>
+
+
+        </div>
+
+
+
+        <div class="div3">
+            <img src="../../img/img1.jpg" alt="img1">
+        </div>
+
+
+
+
+        <div class="div5">
+            <img src="../../img/img3.jpg" alt="img3">
+        </div>
+
+
+        <div class="div6">
+
+            <p>
+                Bienvenidos a una de las pizzerias más destacados en la <strong>Av Huayacan de Cancún!</strong>
+                Nos distinguimos por ofrecer una experiencia gastronomica única y tenebrosa donde convergen los sustos
+                y las sorpresas, con una combinacion de sabores. Somos un lugar ideal para toda la familia, ven a conocernos.
+                <strong>No esperes mas!!</strong>
+            </p>
+
+
+
+        </div>
+
+        <div class="div7">
+
+
+            <h2>Pizzas Fantaso Cancún</h2>
+
+
+            <h2>Contáctanos</h2>
+            <p><strong>Dirección:</strong> Paseos de Maule, Av Huayacan Supermanzana 313 Manzana 243 lote 05, 77533, Cancun, Quintana Roo</p>
+            <p><strong>Teléfono:</strong> +529981483778</p>
+            <p><strong>Email:</strong>pizzafantaso@restaurante.mx</p>
+            <p><strong>Horario:</strong>Lunes a Viernes de 8 a.m - 11 p.m</p>
+
+
+
+
+        </div>
+
+        <div class="div4">
+
+            <footer>
+                <a href="https://web.whatsapp.com/"><img src="../../img/reds1.png" class="logo" alt="reds1"></a>
+                <a href="https://www.facebook.com/"><img src="../../img/reds2.png" class="logo" alt="reds2"></a>
+                <a href="https://www.tiktok.com/es/"><img src="../../img/reds3.png" class="logo" alt="reds3"></a>
+                <a href="https://x.com/?lang=es"><img src="../../img/reds4.png" class="logo" alt="reds4"></a>
+                <a href="https://www.youtube.com/"><img src="../../img/reds5.png" class="logo" alt="reds5"></a>
+                <a href="https://www.instagram.com/"><img src="../../img/reds6.png" class="logo" alt="reds6"></a>
+            </footer>
+
+        </div>
+
+    </div>
+
 </body>
+
 </html>
